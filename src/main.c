fe80cb46b4743fb2f000bdaf5c1949f7de373cbb
Philip <philipgricius@gmail.com>
2026-09-09 20:42:19 +0200

    init

---
+++ src/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(
+        800,
+        600,
+        "Physics Engine",
+        NULL,
+        NULL);
+
+    if (!window)
+    {
+        fprintf(stderr, "Failed to create window\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader(
+            (GLADloadproc)glfwGetProcAddress))
+    {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        return 1;
+    }
+
+    printf("OpenGL: %s\n", glGetString(GL_VERSION));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        double dt = glfwGetTime();
+        glfwSetTime(0);
+
+        mainLoop(dt);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
+
+void mainLoop(double dt)
+{
+}
